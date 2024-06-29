955210f446c1bd89a7dafba46201d8c7f25ec3b1
mrTulsyan <maheep2403@gmail.com>
2024-06-29 17:10:07 +0530

    Updated CORS again

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,14 @@ dotenv.config({
 });
 
 const app = express();
-app.options('*', cors({
-  origin: "https://picturesquare.vercel.app",
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://picturesquare.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 
 const port = process.env.PORT || 5000;
 
